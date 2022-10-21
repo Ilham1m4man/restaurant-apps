ffa6b3da2e846b514b426ef20afd4c1196009118
Ilham1m4man <kucingdiki@gmail.com>
2022-10-21 17:19:27 +0700

    Add aria-label & tabindex to menu

--- src/scripts/components/app-bar.js
+++ src/scripts/components/app-bar.js
@@ -13,10 +13,10 @@ class AppBar extends HTMLElement {
             <div id="header__inner" class="nav-section">
               <h1 id="header__title" tabindex="0">Restaurant Apps</h1>
             </div>
-            <a id="header__menu" class="nav-section">☰</a>
+            <a tabindex="0" id="header__menu" class="nav-section" aria-label="navigation-menu">☰</a>
             <div class="list-container">
                 <ul id="nav__list" class="nav-section">
-                    <li id="nav__item1" class="nav-section"><a href="#home">Home</a></li>
+                    <li id="nav__item1" class="nav-section"><a href="/">Home</a></li>
                     <li id="nav__item2" class="nav-section"><a href="#favourite">Favourite</a></li>
                     <li id="nav__item3" class="nav-section"><a href="https://github.com/Ilham1m4man">About Us</a></li>
                 </ul>
